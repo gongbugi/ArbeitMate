8d99c83310115f894bdf9e4f56d8343a179689dc
KimMinWoo <melonmozzi2@naver.com>
2025-11-07 19:37:19 +0900

    백엔드 프로젝트 생성

---
+++ backend/src/main/java/OpenSourceSW/ArbeitMate/ArbeitMateApplication.java
@@ -0,0 +1,13 @@
+package OpenSourceSW.ArbeitMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbeitMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbeitMateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/OpenSourceSW/ArbeitMate/ArbeitMateApplicationTests.java
@@ -0,0 +1,13 @@
+package OpenSourceSW.ArbeitMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbeitMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
