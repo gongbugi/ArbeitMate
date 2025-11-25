084f528feb65eba68055395bb4ffbd222b443db5
gongbugi <rrobi1492@gmail.com>
2025-11-25 15:41:19 +0900

    근무지 선택 화면 연결

--- frontend/app/screens/WorkplaceSelectScreen.js
+++ frontend/app/screens/WorkplaceSelectScreen.js
@@ -1,30 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import client from "../services/api";
 
-export default function WorkplaceSelectScreen({ navigation }) {
-
+export default function WorkplaceSelectScreen({ navigation, route }) {
+  const [workplaces, setWorkplaces] = useState([]);
+  
   const fetchWorkplaces = async () => {
     try {
-      const res = await fetch("http://13.209.21.34:8080/api/company/my", {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      });
-
-      if (!res.ok) {
-        console.log("근무지 조회 실패");
-        return;
-      }
-
-      const data = await res.json();
-      setWorkplaces(data); // 리스트 저장
-
+      const res = await client.get("근무지 조회 api"); 
+      setWorkplaces(res.data);
     } catch (err) {
-      console.log("서버 오류:", err);
+      console.log("근무지 조회 실패:", err);
     }
   };
 
-  // 화면 처음 로드 or 등록 후 refresh → 자동 새로고침
   useEffect(() => {
     fetchWorkplaces();
   }, [route.params?.refresh]);
@@ -44,7 +34,6 @@ export default function WorkplaceSelectScreen({ navigation }) {
       >
         <Ionicons name="add" size={56} color="#555" />
       </TouchableOpacity>
-
     </View>
   );
 }
@@ -52,12 +41,11 @@ export default function WorkplaceSelectScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#F4F4F5", // 전체 앱 통일 색
+    backgroundColor: "#F4F4F5",
     paddingTop: 80,
     alignItems: "center",
   },
 
-  // 상단 제목
   header: {
     marginBottom: 40,
   },
@@ -68,7 +56,7 @@ const styles = StyleSheet.create({
     color: "#000",
   },
 
-  // 큰 카드 버튼
+
   card: {
     width: 150,
     height: 150,
@@ -76,8 +64,6 @@ const styles = StyleSheet.create({
     borderRadius: 24,
     alignItems: "center",
     justifyContent: "center",
-    
-    // shadow
     shadowColor: "#000",
     shadowOpacity: 0.1,
     shadowOffset: { width: 0, height: 2 },
